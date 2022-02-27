eeb9bc5b15785aa791d62355aa0529b595beb340
github_username <jacobrugano@gmail.com>
2022-02-28 00:15:24 +0300

    Updated the routing module

---
+++ src/app/git-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'; 
+
+
+import { GitFormComponent } from './git-form/git-form.component'; 
+import { QuotesComponent } from './quotes/quotes.component';
+
+
+const routes:Routes = [
+	{ path:"gitUser",component:GitFormComponent }, 
+	{ path:"quotes",component:QuotesComponent } 
+]
+
+
+@NgModule({
+   imports: [
+   CommonModule,
+   RouterModule.forRoot(routes)
+exports: [
+   RouterModule
+]
+})
+
+
+export class GitRoutingModule { }
